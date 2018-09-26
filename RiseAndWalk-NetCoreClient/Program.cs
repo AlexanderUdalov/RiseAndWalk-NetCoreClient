b83481c582c77eae0d0b76c987bea728c46d8028
Александр <alexudalovvvvv@outlook.com>
2018-09-26 10:57:18 +0300

    Init commit. Added base authorization functionality.

--- RiseAndWalk-NetCoreClient/Program.cs
+++ RiseAndWalk-NetCoreClient/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Text;
 
 namespace RiseAndWalk_NetCoreClient
 {
+    class RegisterData
+    {
+        public string Email;
+        public string Password;
+    }
+
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var client = new HttpClient())
+            {
+                GetValues(client);
+
+                var registerData = new RegisterData()
+                {
+                    Email = "udalov@mail.ru",
+                    Password = "A1a$2a3a4a"
+                };
+
+                var bodyString = JsonConvert.SerializeObject(registerData);
+
+                var body = new StringContent(bodyString, Encoding.UTF8, "application/json");
+
+                var postResponse = client.PostAsync("https://localhost:44343/account/login", body).Result;
+                var content = postResponse.Content.ReadAsStringAsync();
+
+                var token = content.Result;
+
+                Console.WriteLine(token);
+
+                using (var authorizatedClient = new HttpClient())
+                {
+                    authorizatedClient.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
+                    GetValues(authorizatedClient);
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static void GetValues(HttpClient client)
+        {
+            try
+            {
+                var _response = client.GetStringAsync("https://localhost:44343/api/values").Result;
+                Console.WriteLine(_response);
+            }
+            catch (AggregateException e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
